4de7fa5c9ad68b77fcaf93be951d2fde5477f038
Fabrizio Giuliano Stocco <fabrizio.stocco@fabriziostocco.xyz>
2023-11-29 00:45:43 +0100

    First Build

---
+++ GoodStore/Models/ItemListing.swift
@@ -0,0 +1,8 @@
+//
+//  ItemListing.swift
+//  GoodStore
+//
+//  Created by MacAdmin on 28/11/23.
+//
+
+import Foundation
---
+++ GoodStore/View Models/ItemListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemListViewModel.swift
+//  GoodStore
+//
+//  Created by MacAdmin on 28/11/23.
+//
+
+import Foundation
