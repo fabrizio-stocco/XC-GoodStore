50106a1caf5ea07f1f2725fc94a9a00786158e12
Fabrizio Giuliano Stocco <fabrizio.stocco@fabriziostocco.xyz>
2023-11-28 21:34:00 +0100

    Initial Commit

---
+++ GoodStore/GoodStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodStoreApp.swift
+//  GoodStore
+//
+//  Created by MacAdmin on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GoodStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
